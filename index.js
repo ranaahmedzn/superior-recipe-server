c17698aada7678e8a041d84e2fd6308b2cd70ec1
Rana-Sheikh <ranasheikhsbd@gmail.com>
2023-05-03 12:48:42 +0600

    loaded recipes based on the chef id

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = 5000;
 app.use(cors())
 
 const chefs = require('./data/chefs.json')
+const recipes = require('./data/recipes.json')
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -13,6 +14,14 @@ app.get('/', (req, res) => {
 
 app.get('/chefs', (req, res) => {
     res.send(chefs)
+})
+
+app.get('/chef-recipes/chef/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    console.log(id)
+
+    const selectedRecipes = recipes.filter(recipe => recipe.chef_id === id)
+    res.send(selectedRecipes)
 })
 
 app.listen(port, () => {
